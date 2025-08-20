a07d007e56f09dfc74f5ca3c3ae105f95b3fe0ba
saritayadav138 <shalu13.mpec@gmial.com>
2025-08-20 10:39:12 +0530

    var having block scope

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,18 @@
 // var a=12;
 // console.log("hii="+a);
-var a=30;
-var a=12;
-console.log("a="+a);
-const a=5;
+// var a=30;
+// var a=12;
+// console.log("a="+a);
+// const a=5;
 
-a=30;
-console.log("a="+a);
+// a=30;
+// console.log("a="+a);
+var a=12;
+
+if(a>10)
+{
+    var a=30;
+    console.log("a="+a);
+
+}
+console.log("a"+a);
