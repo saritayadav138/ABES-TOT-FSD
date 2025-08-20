bd3747b9341a5f82aeb0611516d6ddbe6ebe4b2c
saritayadav138 <shalu13.mpec@gmial.com>
2025-08-20 10:48:35 +0530

    == and ===

--- JS/index.js
+++ JS/index.js
@@ -8,9 +8,20 @@
 // a=30;
 // console.log("a="+a)
 // 
-function sum(a,b)
-{
-return a+b;
-}
-let val=sum(12,20);
-console.log("the sum is"+val);
+// function sum(a,b)
+// {
+// return a+b;
+// }
+// let val=sum(12,20);
+// console.log("the sum is"+val);
+// let a=5;
+// let b="5";
+// if(a==b)
+// {
+//     console.log("hi");
+
+// }
+// else
+// {
+//     console.log("hello");
+// }
