eff127ed7a0651f2ae2b51f68751071d68d15fea
saritayadav138 <shalu13.mpec@gmial.com>
2025-08-21 11:05:18 +0530

    array of object

--- JS/Object/index.js
+++ JS/Object/index.js
@@ -2,10 +2,20 @@ const employee={
     ename:"Rahul Kumar",
     eage:"30",
     eaddress:"Ghaziabad",
-    planguage:{
+    planguage:[
+        
+        {
         lang1:"java",
         lang2:"python"
-    }
+        },
+        {
+        lang1:"java",
+        lang2:"python"
+        },
+        {
+        lang1:"java",
+        lang2:"python"
+        }
+    ]
 }
 console.log(employee);
-console.log(employee.ename);
